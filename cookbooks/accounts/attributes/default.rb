c237f1cb44902c7ead6eb4b7cf03e0c141768b53
Michal Migurski <migurski@fb.com>
2020-01-21 21:41:49 -0800

    Removed default passwd now that it's no longer needed

--- cookbooks/accounts/attributes/default.rb
+++ cookbooks/accounts/attributes/default.rb
@@ -4,6 +4,3 @@ default[:accounts][:manage_home] = true
 
 default[:accounts][:groups] = {}
 default[:accounts][:users] = {}
-
-# Needed in cookbooks/accounts/recipes/default.rb to assign group membership
-default[:etc] = {"passwd": {}}
