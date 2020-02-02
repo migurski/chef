7e9eca5abdf1f70aa26e057a6612194004c23c7f
Jamie Alessio <jamie@urbanfootprint.com>
2020-02-02 21:46:22 +0000

    Add default value for node[:etc][:passwd]

--- cookbooks/accounts/attributes/default.rb
+++ cookbooks/accounts/attributes/default.rb
@@ -4,3 +4,6 @@ default[:accounts][:manage_home] = true
 
 default[:accounts][:groups] = {}
 default[:accounts][:users] = {}
+
+# Needed in cookbooks/accounts/recipes/default.rb to assign group membership
+default[:etc] = {"passwd": {}}
